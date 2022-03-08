ada358e78f3ff7126c57e847442e3f23c60373bd
LU Mian <lumianph@gmail.com>
2022-03-08 11:57:31 +0800

    d

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ copyright = '2022, OpenMLDB'
 author = 'OpenMLDB'
 
 # The full version, including alpha/beta/rc tags
-version = 'dev'
+version = '0.4.2'
 
 
 # -- General configuration ---------------------------------------------------
